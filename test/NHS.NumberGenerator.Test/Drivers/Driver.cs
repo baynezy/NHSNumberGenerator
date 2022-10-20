2a5061dc306710468889850e90e5ddf63e7a6157
Simon Baynes <baynezy@gmail.com>
2022-10-20 11:42:03 +0100

    Add initial skeleton

---
+++ test/NHS.NumberGenerator.Test/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace NHS.NumberGenerator.Test.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ test/NHS.NumberGenerator.Test/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace NHS.NumberGenerator.Test.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
