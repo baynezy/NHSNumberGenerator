36728f6ab70d2872abb337264efde6da12d7eaa5
Simon Baynes <baynezy@gmail.com>
2022-10-20 16:07:40 +0100

    Add NHS number generator
    Closes #19

---
+++ src/NHSNumberGenerator/Generator.cs
@@ -0,0 +1,84 @@
+﻿using System.Text;
+
+namespace NHSNumberGenerator;
+
+public static class Generator
+{
+    public static string Generate()
+    {
+        var number = new NhsNumber();
+        return number.Calculate();
+    }
+
+    private class NhsNumber
+    {
+
+        private readonly Random _random = new();
+        private const int NhsNumberSegments = 9;
+        private List<NumberPart> _numberParts = null!;
+        private readonly int[] _weightings = {10,9,8,7,6,5,4,3,2};
+
+
+        internal NhsNumber()
+        {
+            InitialiseParts();
+        }
+
+        private void InitialiseParts()
+        {
+            _numberParts = new List<NumberPart>();
+            for (var position = 0; position < NhsNumberSegments; position++)
+            {
+                _numberParts.Add(new NumberPart
+                {
+                    Number = RandomDigit(),
+                    Factor = GetPositionFactor(position)
+                });
+            }
+        }
+
+        private int GetPositionFactor(int position)
+        {
+            return _weightings[position];
+        }
+
+        private int RandomDigit()
+        {
+            return _random.Next(0, 10);
+        }
+
+        private class NumberPart
+        {
+            internal int Number { get; init; }
+            internal int Factor { get; init; }
+        }
+
+        internal string Calculate()
+        {
+            var total = _numberParts.Sum(part => part.Number * part.Factor);
+            var remainder = total % 11;
+            
+            var checkDigit = (remainder == 0) ? 0 : 11 - remainder;
+            if (checkDigit == 10) // 10 is not a valid check digit. So must regenerate
+            {
+                InitialiseParts();
+                // ReSharper disable once TailRecursiveCall
+                return Calculate();
+            }
+            
+            _numberParts.Add(new NumberPart
+            {
+                Number = checkDigit,
+                Factor = 1
+            });
+            
+            var calculatedNhsNumber = new StringBuilder();
+            foreach (var numberPart in _numberParts)
+            {
+                calculatedNhsNumber.Append(numberPart.Number);
+            }
+            
+            return calculatedNhsNumber.ToString();
+        }
+    }
+}
---
+++ test/NHSNumberGenerator.Test/Steps/GeneratorFeatureSteps.cs
@@ -0,0 +1,58 @@
+﻿using NHSNumberValidator;
+using Shouldly;
+
+namespace NHSNumberGenerator.Test.Steps;
+
+[Binding]
+public class GeneratorFeatureSteps
+{
+    private readonly List<string> _generated = new();
+
+    [Given(@"A single NHS Number")]
+    public void GivenASingleNhsNumber()
+    {
+        _generated.Add(Generator.Generate());
+    }
+
+    [Then(@"NHS Number should be valid")]
+    public void ThenNhsNumberShouldBeValid()
+    {
+        ValidateNhsNumber(_generated.First());
+    }
+
+    private static void ValidateNhsNumber(string nhsNumber)
+    {
+        nhsNumber.ShouldSatisfyAllConditions(
+            g => Validator.Validate(g).ShouldBeTrue()
+        );
+    }
+
+    [Given(@"Two different NHS Numbers are generated")]
+    public void GivenTwoDifferentNhsNumbersAreGenerated()
+    {
+        _generated.Add(Generator.Generate());
+        _generated.Add(Generator.Generate());
+    }
+
+    [Then(@"NHS Numbers should be valid")]
+    public void ThenNhsNumbersShouldBeValid()
+    {
+        _generated.ForEach(ValidateNhsNumber);
+    }
+
+    [Then(@"NHS Numbers should be different")]
+    public void ThenNhsNumbersShouldBeDifferent()
+    {
+        var duplicateChecker = new Dictionary<string, bool>();
+        
+        _generated.ForEach(nhsNumber =>
+        {
+            if (duplicateChecker.ContainsKey(nhsNumber))
+            {
+                throw new Exception($"Duplicate NHS Number found: {nhsNumber}");
+            }
+
+            duplicateChecker.Add(nhsNumber, true);
+        });
+    }
+}
